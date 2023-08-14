abccbdb297a43d99c2cae97a7a4733edc5361314
jhsseonn <hs125bt@gmail.com>
2023-08-14 20:16:55 +0900

    create project

---
+++ src/main/java/com/dagagam/dagagamweb/DagagamWebApplication.java
@@ -0,0 +1,13 @@
+package com.dagagam.dagagamweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagagamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagagamWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dagagam/dagagamweb/DagagamWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dagagam.dagagamweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagagamWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
